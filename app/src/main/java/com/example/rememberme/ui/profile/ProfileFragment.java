ee7edd12cbf32c98461e7b915c4d2abdac161b1c
gracewanggw <grace.wang.24@dartmouth.edu>
2021-02-18 16:49:27 -0700

    Need to fix gap between view and bar

--- app/src/main/java/com/example/rememberme/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/rememberme/ui/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.rememberme.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,17 +18,25 @@ import com.example.rememberme.R;
 public class ProfileFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
-
+    TextView edit;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        edit = root.findViewById(R.id.edit);
+        edit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+//                Intent intent = new Intent(getContext(),EditProfile.class);
+//                startActivity(intent);
             }
         });
         return root;
