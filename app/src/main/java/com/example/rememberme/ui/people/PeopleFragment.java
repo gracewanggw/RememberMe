fff182444da3bf86f51aef3b3fc7c43ec5baa659
gracewanggw <grace.wang.24@dartmouth.edu>
2021-02-19 16:21:50 -0700

    People fragment implemented
    Need data in data source to see if it actually works.

--- app/src/main/java/com/example/rememberme/ui/people/PeopleFragment.java
+++ app/src/main/java/com/example/rememberme/ui/people/PeopleFragment.java
@@ -1,12 +1,18 @@
 package com.example.rememberme.ui.people;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,61 +21,103 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.rememberme.DB.FramilyDbSource;
 import com.example.rememberme.Framily;
+import com.example.rememberme.FramilyProfile;
 import com.example.rememberme.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PeopleFragment extends Fragment {
 
+    GridView gridView;
+    List<Framily> people;
+
+    FramilyDbSource dataSource;
+
+    public static final String FRAMILY_KEY = "framily key";
+    
     private HomeViewModel homeViewModel;
 
-    CustomAdapter customAdapter;
-    List<Framily> values;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_people, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        gridView = root.findViewById(R.id.people_grid_view);
+
+        updateView();
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        updateView();
+        super.onResume();
     }
 
     public void updateView(){
 
+        dataSource = new FramilyDbSource(getActivity().getApplicationContext());
+        dataSource.open();
+
+        people = dataSource.fetchEntries();
+
+        CustomAdapter customAdapter = new CustomAdapter(people);
+        gridView.setAdapter(customAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getActivity(), FramilyProfile.class);
+                intent.putExtra(FRAMILY_KEY,customAdapter.getItem(position).getId());
+                startActivity(intent);
+            }
+        });
+
     }
 
-    private class CustomAdapter extends ArrayAdapter<Framily> {
-        private int resourceLayout;
-        private Context mContext;
+    // adapter to show grid view of people and their names and relationship from database
+    private class CustomAdapter extends BaseAdapter{
         List<Framily> framilies;
 
-        public CustomAdapter(Context context, int resource, List<Framily> entries) {
-            super(context, resource, entries);
-            this.resourceLayout = resource;
-            this.mContext = context;
-            this.framilies = entries;
+        public CustomAdapter(List<Framily> people){
+            framilies = people;
         }
 
+        @Override
+        public int getCount() {
+            return framilies.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
         public Framily getItem(int position){
             return framilies.get(position);
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View v = getLayoutInflater().inflate(R.layout.entry_view,null);
+            View v = getLayoutInflater().inflate(R.layout.people_grid_item,null);
+
+            ImageView imageView = v.findViewById(R.id.framily_Image);
+            TextView nameView = v.findViewById(R.id.framily_Name);
+            TextView relationView = v.findViewById(R.id.framily_Relationship);
 
             Framily fram = framilies.get(position);
+            int image = fram.getImage();
+            String name = fram.getNameFirst();
+            String relationship = fram.getRelationship();
 
-
-
+            imageView.setImageResource(image);
+            nameView.setText(name);
+            relationView.setText(relationship);
 
             return v;
         }
