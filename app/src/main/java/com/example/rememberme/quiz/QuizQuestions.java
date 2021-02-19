8d94940442e2f86cd1b4108c633b04b6f45bc805
danielle-fang <25496850+danielle-fang@users.noreply.github.com>
2021-02-19 15:51:40 -0500

    working through the quiz things

--- app/src/main/java/com/example/rememberme/quiz/QuizQuestions.java
+++ app/src/main/java/com/example/rememberme/quiz/QuizQuestions.java
@@ -1,4 +1,24 @@
 package com.example.rememberme.quiz;
 
 public class QuizQuestions {
+
+    String question ,
+            op1 ,
+            op2 ,
+            op3 ,
+            op4 ,
+            correct_answer;
+
+    QuizQuestions (String question , String op1 , String op2 ,String op3, String op4 , String corrAns) {
+        this.question = question ;
+        this.op1 = op1;
+        this.op2 = op2;
+        this.op3 = op3 ;
+        this.op4 = op4 ;
+        this.correct_answer = corrAns ;
+    }
+
+    public QuizQuestions getQuestion(){
+        return this;
+    }
 }
