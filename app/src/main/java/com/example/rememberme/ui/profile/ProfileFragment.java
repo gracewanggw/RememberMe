9508a8694725d58df570ecd7792896a584e76a98
gracewanggw <grace.wang.24@dartmouth.edu>
2021-02-21 20:51:51 -0600

    profile fragment updates

--- app/src/main/java/com/example/rememberme/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/rememberme/ui/profile/ProfileFragment.java
@@ -1,10 +1,13 @@
 package com.example.rememberme.ui.profile;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,9 +16,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.rememberme.EditFramilyProfile;
 import com.example.rememberme.R;
+import com.example.rememberme.RoundImage;
 
 public class ProfileFragment extends Fragment {
+    ImageView photo;
+    RoundImage roundedImage;
 
     private NotificationsViewModel notificationsViewModel;
     TextView edit;
@@ -24,19 +31,18 @@ public class ProfileFragment extends Fragment {
         notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-//        final TextView textView = root.findViewById(R.id.text_notifications);
-//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+
+        photo = root.findViewById(R.id.photo_profile);
+        Bitmap bm = BitmapFactory.decodeResource(getResources(),R.drawable._pic);
+        roundedImage = new RoundImage(bm);
+        photo.setImageDrawable(roundedImage);
+
         edit = root.findViewById(R.id.edit);
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(getContext(),EditProfile.class);
-//                startActivity(intent);
+                Intent intent = new Intent(getContext(), EditFramilyProfile.class);
+                startActivity(intent);
             }
         });
         return root;
