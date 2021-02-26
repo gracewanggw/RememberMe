73a159eb26cec8e52c2e2e6778b83b358a514f1e
gracewanggw <grace.wang.24@dartmouth.edu>
2021-02-25 23:58:02 -0600

    UI updates

--- app/src/main/java/com/example/rememberme/ui/quiz/QuizFragment.java
+++ app/src/main/java/com/example/rememberme/ui/quiz/QuizFragment.java
@@ -1,19 +1,17 @@
 package com.example.rememberme.ui.quiz;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Switch;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.rememberme.R;
@@ -27,7 +25,10 @@ public class QuizFragment extends Fragment {
     LinearLayout birthdays;
     LinearLayout review;
 
-    Switch fib;
+    Button fill_in_blank;
+    Button mult_choice;
+
+    boolean fib;
 
     public static final String QUIZ_TYPE_KEY = "quiz type";
     public static final String FILL_IN_BLANK = "fill in blank";
@@ -42,45 +43,94 @@ public class QuizFragment extends Fragment {
         nameFace = root.findViewById(R.id.name_to_face);
         birthdays = root.findViewById(R.id.quiz_birthdays);
         review = root.findViewById(R.id.quiz_review);
-        fib = root.findViewById(R.id.fill_in_blank);
+        fill_in_blank = root.findViewById(R.id.fill_in_blank);
+        mult_choice = root.findViewById(R.id.mc_button);
 
-        quizAll.setOnClickListener(new View.OnClickListener() {
+        fill_in_blank.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                fib = true;
+                mult_choice.setBackgroundResource(R.drawable.quiz_button1);
+                fill_in_blank.setBackgroundResource(R.drawable.quiz_button2);
+                fill_in_blank.setTextColor(Color.WHITE);
+                mult_choice.setTextColor(Color.BLACK);
+            }
+        });
+
+        mult_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fib = false;
+                mult_choice.setBackgroundResource(R.drawable.quiz_button2);
+                fill_in_blank.setBackgroundResource(R.drawable.quiz_button1);
+                fill_in_blank.setTextColor(Color.BLACK);
+                mult_choice.setTextColor(Color.WHITE);
+            }
+        });
+
+        quizAll.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    quizAll.setAlpha((float)0.4);
+                    v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    quizAll.setAlpha((float)1);
 //                Intent intent = new Intent(getContext(),QuizActivity.class);
-//                intent.putExtra(FILL_IN_BLANK,fib.isSelected())
+//                intent.putExtra(FILL_IN_BLANK,fib);
 //                intent.putExtra(QUIZ_TYPE_KEY,"all");
+                }
+                return true;
             }
         });
 
-        nameFace.setOnClickListener(new View.OnClickListener() {
+        nameFace.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    nameFace.setAlpha((float)0.4);
+                    v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    nameFace.setAlpha((float)1);
 //                Intent intent = new Intent(getContext(),QuizActivity.class);
-//                intent.putExtra(FILL_IN_BLANK,fib.isSelected())
+//                intent.putExtra(FILL_IN_BLANK,fib);
 //                intent.putExtra(QUIZ_TYPE_KEY,"name to face");
+                }
+                return true;
             }
         });
 
-        birthdays.setOnClickListener(new View.OnClickListener() {
+        birthdays.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    birthdays.setAlpha((float)0.4);
+                    v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    birthdays.setAlpha((float)1);
 //                Intent intent = new Intent(getContext(),QuizActivity.class);
-//                intent.putExtra(FILL_IN_BLANK,fib.isSelected())
+//                intent.putExtra(FILL_IN_BLANK,fib);
 //                intent.putExtra(QUIZ_TYPE_KEY,"birthdays");
+                }
+                return true;
             }
         });
 
-        review.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        review.setOnTouchListener(new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                review.setAlpha((float)0.4);
+                v.playSoundEffect(android.view.SoundEffectConstants.CLICK);
+            } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                review.setAlpha((float)1);
 //                Intent intent = new Intent(getContext(),QuizActivity.class);
-//                intent.putExtra(FILL_IN_BLANK,fib.isSelected())
+//                intent.putExtra(FILL_IN_BLANK,fib);
 //                intent.putExtra(QUIZ_TYPE_KEY,"review");
             }
-        });
-
+            return true;
+        }
+    });
         return root;
-    }
-
+}
 }
