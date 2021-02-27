151256499b5d320ea63fa5eb20ef32af47532b68
gracewanggw <grace.wang.24@dartmouth.edu>
2021-02-27 13:12:48 -0600

    working on walkthrough screen

---
+++ app/src/main/java/com/example/rememberme/WalkthroughActivity.java
@@ -0,0 +1,14 @@
+package com.example.rememberme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WalkthroughActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_walkthrough);
+    }
+}
